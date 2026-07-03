368f85c5f23cf1471c4cf1311f83bf017b9a4ce4
Guilherme Meato <guimeato@gmail.com>
2026-07-03 19:08:18 -0300

    feat: aplica formatação aos valores dos cards de crypto

--- src/components/CryptoCard.js
+++ src/components/CryptoCard.js
@@ -1,14 +1,16 @@
 import { getTopCrypto } from "../services/api.js";
+import { formatCurrency } from "../utils/formatCurrency.js";
+
 
 export function cryptoCard(crypto){
   return (
     `
      <div class="crypto-card">
         <h1>${crypto.name}</h1>
-        <p>${crypto.current_price}</p>
-        <p>${crypto.market_cap}</p>
-        <p>${crypto.high_24h}</p>
-        <p>${crypto.low_24h}</p>
+        <p>${formatCurrency(crypto.current_price)}</p>
+        <p>${formatCurrency(crypto.market_cap)}</p>
+        <p>${formatCurrency(crypto.high_24h)}</p>
+        <p>${formatCurrency(crypto.low_24h)}</p>
       </div>
     `
   );
