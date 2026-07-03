e12fc30545816ddd5fffaf5e7eacd651b0560b58
Guilherme Meato <guimeato@gmail.com>
2026-07-03 11:18:49 -0300

    feat: cria requisições getCrypto e getTopCrypto

---
+++ src/services/api.js
@@ -0,0 +1,32 @@
+import { API_KEY } from '../config.js'
+
+const BASE_URL = "https://api.coingecko.com/api/v3";
+
+export async function getTopCrypto(){
+  try{
+    const response = await fetch(`${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false&x_cg_demo_api_key=${API_KEY}`);
+
+    const data = await response.json();
+
+    return data;
+  }
+  catch(error){
+    console.log('Erro ao carregar as principais cryptos');
+  }
+  
+}
+
+export async function getCrypto(id) {
+  try{
+    const response = await fetch(`${BASE_URL}/coins/${id}?x_cg_demo_api_key=${API_KEY}`);
+
+    const data = await response.json();
+
+    return data;
+  }
+  catch(error){
+    console.log(`Erro ao carregar a crypto ${id}`);
+  }
+  
+}
+
