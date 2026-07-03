1a11546acec5fc03c693f71b25c84da88441108f
Guilherme Meato <guimeato@gmail.com>
2026-07-03 18:55:12 -0300

    feat: aplica formatação aos detalhes das cryptos

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -1,4 +1,6 @@
 import { getCrypto } from "../services/api.js";
+import { formatCurrency } from "../utils/formatCurrency.js";
+import { formatNumber } from "../utils/formatNumber.js";
 
 export function cryptoDetails(crypto, currency = "usd"){
   
@@ -9,15 +11,15 @@ export function cryptoDetails(crypto, currency = "usd"){
 
       <h1>${crypto.name} (${crypto.symbol.toUpperCase()})</h1>
 
-      <p>Preço atual: ${crypto.market_data.current_price[currency]}</p>
-      <p>Capitalização: ${crypto.market_data.market_cap[currency]}</p>
-      <p>Maior preço (24h): ${crypto.market_data.high_24h[currency]}</p>
-      <p>Menor preço (24h): ${crypto.market_data.low_24h[currency]}</p>
+      <p>Preço atual: ${formatCurrency(crypto.market_data.current_price[currency], currency)}</p>
+      <p>Capitalização: ${formatCurrency(crypto.market_data.market_cap[currency], currency)}</p>
+      <p>Maior preço (24h): ${formatCurrency(crypto.market_data.high_24h[currency], currency)}</p>
+      <p>Menor preço (24h): ${formatCurrency(crypto.market_data.low_24h[currency], currency)}</p>
 
       <p>Ranking: #${crypto.market_cap_rank}</p>
-      <p>Oferta circulante: ${crypto.market_data.circulating_supply}</p>
-
-      <p>Variação 24h: ${crypto.market_data.price_change_percentage_24h}%</p>
+      
+      <p>Oferta circulante: ${formatNumber(crypto.market_data.circulating_supply, currency)}</p>
+      <p>Variação 24h: ${crypto.market_data.price_change_percentage_24h.toFixed(3)}%</p>
 
       <button id="back-btn">Voltar</button>
     </section>
