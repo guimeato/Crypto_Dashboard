4b3c57667e7099369ebf21ff4d7eb4e3ed4e6323
Guilherme Meato <guimeato@gmail.com>
2026-07-03 22:31:39 -0300

    feat: add suporte a múltiplas moedas nos CryptoCards

--- src/components/CryptoCard.js
+++ src/components/CryptoCard.js
@@ -2,15 +2,15 @@ import { getTopCrypto } from "../services/api.js";
 import { formatCurrency } from "../utils/formatCurrency.js";
 
 
-export function cryptoCard(crypto){
+export function cryptoCard(crypto, currency = 'usd'){
   return (
     `
      <div class="crypto-card">
         <h1>${crypto.name}</h1>
-        <p>${formatCurrency(crypto.current_price)}</p>
-        <p>${formatCurrency(crypto.market_cap)}</p>
-        <p>${formatCurrency(crypto.high_24h)}</p>
-        <p>${formatCurrency(crypto.low_24h)}</p>
+        <p>${formatCurrency(crypto.current_price, currency)}</p>
+        <p>${formatCurrency(crypto.market_cap, currency)}</p>
+        <p>${formatCurrency(crypto.high_24h, currency)}</p>
+        <p>${formatCurrency(crypto.low_24h, currency)}</p>
       </div>
     `
   );
