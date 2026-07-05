37a5a834b3589eb38c66c64c9cb8742b5b853b83
Guilherme Meato <guimeato@gmail.com>
2026-07-04 21:43:13 -0300

    feat: add layout da HomePage

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+export function Header(){
+  return(
+    `
+    <header>
+      <div class="left-section"><strong>Crypto Dashboard</strong></div>
+      <div class="right-section">
+        <a>oi</a>
+        <a>tchau</a>
+      </div>
+    </header>
+    `
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
 import { getTopCrypto } from "../services/api.js";
 import { cryptoCard } from "../components/CryptoCard.js";
+import { Header } from "../components/Header.js";
 
 export async function HomePage(currency = 'usd'){
   const Topcryptos = await getTopCrypto(currency);
 
-  const cryptos = Topcryptos.map((crypto)=>{
-    return cryptoCard(crypto, currency);
-  }).join("");
-
-
-  return cryptos;
+  return `
+    ${Header()}
+    
+    <main class="container">
+      ${Topcryptos.map((crypto)=>{
+        return cryptoCard(crypto, currency);}).join("")}
+    </main>
+  `
 }
