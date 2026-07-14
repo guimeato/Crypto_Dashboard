aa46474824a855964d90f0dc31f3cab6ee792fc4
Guilherme Meato <guimeato@gmail.com>
2026-07-13 22:25:13 -0300

    feat: add estilo do CryptoCard

--- src/components/CryptoCard.js
+++ src/components/CryptoCard.js
@@ -1,15 +1,33 @@
 import { formatCurrency } from "../utils/formatCurrency.js";
 
 export function cryptoCard(crypto, currency = 'usd'){
+  const change = crypto.price_change_percentage_24h.toFixed(2);
+
+  const changeClass = change > 0 ? "positive" : change < 0 ? "negative" : "neutral";
+  
+  const icon = change > 0 ? "▲" : change < 0  ? "▼" : "•";
+  
+
   return (
     `
-      <article class="crypto-card">
-        <a href="#/crypto/${crypto.id}">
-          <h2>${crypto.name}</h2>
-          <p>Preço: ${formatCurrency(crypto.current_price, currency)}</p>
-          <p>Market Cap: ${formatCurrency(crypto.market_cap, currency)}</p>
-          <p>Máxima (24h): ${formatCurrency(crypto.high_24h, currency)}</p>
-          <p>Mínima (24h): ${formatCurrency(crypto.low_24h, currency)}</p>
+      <article>
+        <a class="crypto-card" href="#/crypto/${crypto.id}">
+          <img src="${crypto.image}">
+          <div class="crypto-card-info">
+
+            <div class="crypto-card-symbol">
+              <strong>${crypto.symbol.toUpperCase()}</strong>
+              <p>/ ${currency.toUpperCase()}</p>
+            </div>
+            
+            <p class="crypto-card-title">${crypto.name}</p>
+            
+          </div>
+          
+          <p class="current-price">${formatCurrency(crypto.current_price, currency)}</p>
+          
+          <p class="${changeClass}">${icon} ${crypto.price_change_percentage_24h.toFixed(2)}%</p>
+           
         </a>
       </article>
     `
