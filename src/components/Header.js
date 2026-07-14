17e1fbecf95cc716e918ecee1aab9c74ab23b13b
Guilherme Meato <guimeato@gmail.com>
2026-07-13 22:28:19 -0300

    feat: add estilo do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
+import { SearchInput } from "./SearchInput.js";
+
 export function Header(){
   return(
     `
     <header>
-      <div class="left-section"><strong>Crypto Dashboard</strong></div>
-      <div class="right-section">
-        <a>oi</a>
-        <a>tchau</a>
+      <div class="header-container">
+        <div class="left-section">
+          <a href="#/home"><strong>Crypto Dashboard</strong></a>
+        </div>
+        <div class="middle-section">
+          ${SearchInput()}
+        </div>
+        <div class="right-section">
+          <a href="#/favoritos">⭐ Favoritos</a>
+        </div>
       </div>
+      
     </header>
     `
   );
---
+++ src/components/SearchInput.js
@@ -0,0 +1,10 @@
+export function SearchInput(){
+  return(`
+    <input
+    type="search" 
+    placeholder="Pesquisar Cryptomoeda..."
+    autocomplete="off"
+    aria-label = "Pesquisar Cryptomoeda"
+     >
+    `);
+}
